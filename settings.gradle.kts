90ea718cbe333aeaca8a7d8eff2c8be8c9c52d07
z.murodov <z.murodov@tbank.ru>
2026-06-03 18:48:14 +0300

    build: add Gradle wrapper (8.10.2) and settings

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+rootProject.name = "openapi-test-framework"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
+
+include("core", "demo")
